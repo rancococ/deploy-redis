f935d0169fc6602b2e2fa0dd77b31c5ef4580595
rancococ@qq.com <rancococ@qq.com>
2020-04-22 14:54:24 +0800

    更新redis

--- source/deploy.sh
+++ source/deploy.sh
@@ -2,29 +2,21 @@
 
 ##########################################################################
 # deploy.sh
-# --load   : 加载镜像
-# --init   : 初始目录
+# --init   : init dir
+# --load   : load images
 ##########################################################################
 
+# set -x
 set -e
 
-#
 # set author info
-#
 date1=`date "+%Y-%m-%d %H:%M:%S"`
 date2=`date "+%Y%m%d%H%M%S"`
 author="yong.ran@cdjdgm.com"
 
-#
-# envirionment
-#
-
-
 set -o noglob
 
-#
 # font and color 
-#
 bold=$(tput bold)
 underline=$(tput sgr 0 1)
 reset=$(tput sgr0)
@@ -35,9 +27,7 @@ yellow=$(tput setaf 3)
 blue=$(tput setaf 4)
 white=$(tput setaf 7)
 
-#
 # header and logging
-#
 header() { printf "\n${underline}${bold}${blue}> %s${reset}\n" "$@"; }
 header2() { printf "\n${underline}${bold}${blue}>> %s${reset}\n" "$@"; }
 info() { printf "${white}➜ %s${reset}\n" "$@"; }
@@ -51,9 +41,7 @@ trap "error '******* Caught sigint signal. Stopping...*******'; exit 2" sigint
 
 set +o noglob
 
-#
 # entry base dir
-#
 pwd=`pwd`
 base_dir="${pwd}"
 source="$0"
@@ -65,41 +53,35 @@ done
 base_dir="$( cd -P "$( dirname "$source" )" && pwd )"
 cd "${base_dir}"
 
-#
+# envirionment
+
 # args flag
-#
 arg_help=
-arg_load=
 arg_init=
+arg_load=
+arg_images_package=
 arg_empty=true
 
-arg_package=
-
-#
 # parse parameter
-#
 # echo $@
-# 定义选项， -o 表示短选项 -a 表示支持长选项的简单模式(以 - 开头) -l 表示长选项 
-# a 后没有冒号，表示没有参数
-# b 后跟一个冒号，表示有一个必要参数
-# c 后跟两个冒号，表示有一个可选参数(可选参数必须紧贴选项)
-# -n 出错时的信息
-# -- 也是一个选项，比如 要创建一个名字为 -f 的目录，会使用 mkdir -- -f ,
-#    在这里用做表示最后一个选项(用以判定 while 的结束)
-# $@ 从命令行取出参数列表(不能用用 $* 代替，因为 $* 将所有的参数解释成一个字符串
-#                         而 $@ 是一个参数数组)
+# define options, -o : short options, -a : simple mode for long options (starts with -), -l : long options
+# no colon after, indicating no parameter
+# followed by a colon to indicate that there is a required parameter
+# followed by two colons to indicate that there is an optional parameter (the optional parameter must be next to the option)
+# -n information on error
+# -- it is also an option. for example, to create a directory named "-f", "mkdir -- -f" will be used
+# $@ take the parameter list from the command line
 # args=`getopt -o ab:c:: -a -l apple,banana:,cherry:: -n "${source}" -- "$@"`
-args=`getopt -o h -a -l help,load:,init -n "${source}" -- "$@"`
-# 判定 getopt 的执行时候有错，错误信息输出到 STDERR
+args=`getopt -o h -a -l help,init,load: -n "${source}" -- "$@"`
+# terminate the execution when there is an error in the execution of getopt
 if [ $? != 0 ]; then
-    error "Terminating..." >&2
+    error "terminating..." >&2
     exit 1
 fi
 # echo ${args}
-# 重新排列参数的顺序
-# 使用eval 的目的是为了防止参数中有shell命令，被错误的扩展。
+# reorder parameters(The purpose of using eval is to prevent the shell command in the parameter from being extended by mistake)
 eval set -- "${args}"
-# 处理具体的选项
+# handling specific options
 while true
 do
     case "$1" in
@@ -109,54 +91,46 @@ do
             arg_empty=false
             shift
             ;;
-        --load | -load)
-            info "option --load argument : $2"
-            arg_load=true
-            arg_empty=false
-            arg_package=$2
-            shift 2
-            ;;
         --init | -init)
             info "option --init"
             arg_init=true
             arg_empty=false
             shift
             ;;
+        --load | -load)
+            info "option --load argument : $2"
+            arg_load=true
+            arg_empty=false
+            arg_images_package=$2
+            shift 2
+            ;;
         --)
             shift
             break
             ;;
         *)
-            error "Internal error!"
+            error "internal error!"
             exit 1
             ;;
     esac
 done
-#显示除选项外的参数(不包含选项的参数都会排到最后)
-# arg 是 getopt 内置的变量 , 里面的值，就是处理过之后的 $@(命令行传入的参数)
+# display parameters other than options (parameters without options will be last)
+# arg is the built-in variable of getopt. the value in arg is $@ (the parameter passed in from the command line) after processing
 for arg do
    warn "$arg";
 done
 
+##########################################################################
+
 # show usage
-usage=$"`basename $0` [-h|--help] [--load=xxx.tgz] [--init]
+usage=$"`basename $0` [-h|--help] [--init] [--load=xxx.tgz]
        [-h|--help]
                        show help info.
-       [--load=xxx.tgz]
-                       load images.
        [--init]
                        init volume.
+       [--load=xxx.tgz]
+                       load images.
 "
-
-
-# load images
-fun_load_images() {
-    header "load images : "
-    info "load image : ${arg_package}"
-    docker load -i "${arg_package}";
-    success "successfully loaded ${arg_package}"
-    return 0
-}
 
 # init volume
 fun_init_volume() {
@@ -191,6 +165,14 @@ fun_init_volume() {
     return 0
 }
 
+# load images
+fun_load_images() {
+    header "load images : "
+    info "load image : ${arg_images_package}"
+    docker load -i "${arg_images_package}";
+    success "successfully loaded ${arg_images_package}"
+    return 0
+}
 
 ##########################################################################
 
@@ -206,20 +188,21 @@ if [ "x${arg_help}" == "xtrue" ]; then
     exit 1
 fi
 
+# init volume
+if [ "x${arg_init}" == "xtrue" ]; then
+    fun_init_volume;
+fi
+
 # load images
 if [ "x${arg_load}" == "xtrue" ]; then
-    if [ ! -f ${arg_package} ]; then
+    if [ ! -f ${arg_images_package} ]; then
         usage "$usage";
         exit 1
     fi
     fun_load_images;
 fi
 
-# init volume
-if [ "x${arg_init}" == "xtrue" ]; then
-    fun_init_volume;
-fi
 
 echo ""
 
-exit $?
+# exit $?
